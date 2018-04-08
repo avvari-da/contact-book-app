f7e8943e9105fdbcbcd99fa2bb3fb88a9b81ebbc
Dheeraj Avvari <dj.mech.iitkgp@gmail.com>
2018-04-08 11:58:01 +0530

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,10 @@ module.exports = {
             inject: 'body'
         }),
         new webpack.DefinePlugin({
-            'API_BASE_URL': JSON.stringify(process.env.API_BASE_URL || 'http://localhost:3000/api/v1')
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+                'API_BASE_URL': JSON.stringify(process.env.API_BASE_URL || 'http://localhost:3000')
+            }
         })
     ],
     devServer: {
